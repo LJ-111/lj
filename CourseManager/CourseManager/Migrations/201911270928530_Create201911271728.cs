809281a3d4c4d7d22a787a3b7f6488ea96e8de9c
218368943 <2818545636@qq.com>
2019-12-13 15:01:15 +0800

    12.13

---
+++ CourseManager/CourseManager/Migrations/201911270928530_Create201911271728.cs
@@ -0,0 +1,48 @@
+namespace CourseManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Create201911271728 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Classes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                        TeacherId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Teachers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                        ClassId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+            DropTable("dbo.Teachers");
+            DropTable("dbo.Classes");
+        }
+    }
+}
